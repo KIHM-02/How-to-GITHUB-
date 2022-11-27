14407be792a39636ee94826c8077b2ae209ed355
Kevin Isaac Montero Huerta <81937772+KIHM-02@users.noreply.github.com>
2022-11-27 01:26:22 -0600

    Added cases to switch for every tasks
    Added the task for every collaborator

--- GitHub/main.c
+++ GitHub/main.c
@@ -7,7 +7,8 @@ int main()
 {
     int opcion = 0;
 
-    printf("Ingrese la opcion deseada:\n 1)Como crear un repositorio\n 2)Como agregar un companiero\n 5)Salir\n");
+    printf("Ingrese la opcion deseada:\n 1)Como crear un repositorio\n 2)Como agregar un companiero\n 8)Salir\n");
+    printf("");
         scanf("%d", &opcion);
 
     switch (opcion)
@@ -19,11 +20,32 @@ int main()
         case 2:
             agregar_usuarios_repositorio();
         break;
+
+        case 3:
+           //poner aqui la libreria de COMMITS y pull request
+        break;
+
+        case 4:
+             //poner aqui la funcion de la libreria ANGEL
+        break;
+
+        case 5:
+            //poner aqui la funcion de la libreria YAEL
+        break;
+
+        case 6:
+            //poner aqui la funcion de la libreria ROMAN
+        break;
+
+        case 7:
+            //poner aqui la funcion de la libreria PETTERSON
+        break;
         
         default: break;
     }
 
-    printf("\n----*Hasta luego***----\n");
+    system("cls");
+    printf("\n----***Hasta luego***----\n");
     getch();
     return 0;
 }
