040019332f4dea78c656ab1bac01a0f797f0a099
Kevin Isaac Montero Huerta <81937772+KIHM-02@users.noreply.github.com>
2022-11-27 22:01:14 -0600

    Added the files of teamwork
    Added teamwork library and photos.

---
+++ GitHub/solidifica_trabajo_equipo.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void solidifica_trabajo_equipo()
+{
+    printf("Para agregar tu trbajo colaborativo y mostrar tu parte : \n\n");
+
+    printf("Presionar el boton verde con el nombre code y seleccionar Download Zip \n");
+    system("imagenes\\solidificar_trabajo_en_equipos.png");
+    system("pause");
+
+    printf("\n Posterior a eso ya descargado solo trabajaras tu archivo y lo agregaras a la carpeta\n");
+    system("imagenes\\solidificar_trabajo_en_equipo2.png");
+    system("pause");
+
+    printf("\n Ya por ultimo solo uniras tu parte en el apartado de 'Add File' y guardas los cambios \n");
+    system("imagenes\\solidificar_trabajo_en_equipo3.png");
+    system("pause");
+
+    printf("\n Ya habras subido tu parte de forma satisfactoria \n");
+    system("pause");
+    system("cls");
+}
