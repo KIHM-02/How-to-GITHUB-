bcda94776a16a2187fc639d76744e8bde2f51a8d
RACM0704 <119090432+RACM0704@users.noreply.github.com>
2022-11-27 21:26:23 -0600

    agregue archivos de solidificacion
    meti mi codigo y todos los archivos para hacer actualizacion de todos los archivos

--- GitHub/main.c
+++ GitHub/main.c
@@ -1,48 +1,63 @@
 #include "agregar_usuarios.h"
 #include "crear_repositorio.h"
+#include "crear_pull_request.h"
+#include "historial_cambios.h"
+#include "solidifica_trabajo_equipo.h"
 
 #include <conio.h>
-
+#include <stdbool.h>
 int main()
 {
     int opcion = 0;
+    
+    bool ban = false;  
 
-    printf("Ingrese la opcion deseada:\n 1)Como crear un repositorio\n 2)Como agregar un companiero\n 8)Salir\n");
-    printf("");
+    while (ban==false)
+    {
+        printf("Ingrese la opcion deseada:\n 1)Como crear un repositorio\n 2)Como agregar un companiero\n 3)Como hacer Commits y Pull request\n 4)Historial de Cambios\n 6)Solidifica como hacer el trabajo en equipo\n 8)Salir\n\n");
+        
         scanf("%d", &opcion);
 
-    switch (opcion)
-    {
-        case 1:
-            explicacion_de_creacion();
-        break;
+            switch (opcion)
+            {
+                case 1:
+                    explicacion_de_creacion();
+                break;
 
-        case 2:
-            agregar_usuarios_repositorio();
-        break;
+                case 2:
+                    agregar_usuarios_repositorio();
+                break;
 
-        case 3:
-           //poner aqui la libreria de COMMITS y pull request
-        break;
+                case 3:
+                    creacion_de_pullRequest();
+                break;
 
-        case 4:
-             //poner aqui la funcion de la libreria ANGEL
-        break;
+                case 4:
+                    ver_historial_cambios();
+                break;
 
-        case 5:
-            //poner aqui la funcion de la libreria YAEL
-        break;
+                case 5:
+                    //poner aqui la funcion de la libreria YAEL
+                break;
 
-        case 6:
-            //poner aqui la funcion de la libreria ROMAN
-        break;
+                case 6:
+                    solidifica_trabajo_equipo();
+                break;
 
-        case 7:
-            //poner aqui la funcion de la libreria PETTERSON
-        break;
-        
-        default: break;
+                case 7:
+                    
+                break;
+                
+                case 8:
+                    ban = true;
+                break;
+                
+                default: break;
+            }
+
     }
+    
+    
 
     system("cls");
     printf("\n----***Hasta luego***----\n");
---
+++ GitHub/solidifica_trabajo_equipo.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void solidifica_trabajo_equipo()
+{
+    printf("Para agregar tu trbajo colaborativo y mostrar tu parte : \n\n");
+
+    printf("Presionar el boton verde con el nombre code y seleccionar Download Zip \n");
+    system("imagenes\\solidificar_trabajo_en_equipos.png");
+    system("pause");
+
+    printf("\n Posterior a eso ya descargado solo trabajaras tu archivo y lo agregaras a la carpeta\n");
+    system("imagenes\\solidificar_trabajo_en_equipo2.png");
+    system("pause");
+
+    printf("\n Ya por ultimo solo sunirias tu parte en el apartado de Add File y guardas los cambios \n");
+    system("imagenes\\solidificar_trabajo_en_equipo3.png");
+    system("pause");
+
+    printf("\n Ya abras subido tu parte de forma satifacctoria \n");
+    system("pause");
+}
